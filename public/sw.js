e89dda5f366ed2a32cf61c2796b52010ed935236
verdaguer1805 <canpitallat@gmail.com>
2026-09-11 18:30:14 +0200

    Presenta el stock como tarjetas en movil

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "cma-v115";
+const CACHE = "cma-v116";
 const APP_SHELL = ["./", "./index.html", "./admin.html", "./icon.svg", "./falck-eagle-admin.png"];
 
 self.addEventListener("install", (event) => {
